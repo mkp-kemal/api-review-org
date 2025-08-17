c8b5997fc1357479291dfcfb4580402b78daa5fa
Kemal Pasha <kemal.garut7@gmial.com>
2025-08-17 12:10:21 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://127.0.0.1:5500'], // atau ['*'] untuk semua origin
+    origin: [process.env.APP_URL_CORS],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
